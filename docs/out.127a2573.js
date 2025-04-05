7cb74f10b7c2a61a8f46e51e67437e853b08b666
belubruh123 <111185821+belubruh123@users.noreply.github.com>
2025-04-05 09:59:04 +0800

    add docs page generated by jsdoc.

---
+++ docs/out.127a2573.js
@@ -0,0 +1,2 @@
+(()=>{let e,t,s,l,n=document.getElementsByClassName("prettyprint source linenums"),a=0,i=0;if(n&&n[0])for(l=document.location.hash.substring(1),s=(t=n[0].getElementsByTagName("li")).length;a<s;a++)i++,e=`line${i}`,t[a].id=e,e===l&&(t[a].className+=" selected")})();
+//# sourceMappingURL=out.127a2573.js.map
